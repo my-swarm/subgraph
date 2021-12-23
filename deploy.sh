8190f04d50472c3e122cf6c0a5db0cd58a111c48
Jiri Blaha <blaha@wedecom.cz>
2021-12-23 14:05:01 +0100

    Mumbai subgraph

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,11 @@ GRAPH_NAME="my-swarm/issuance"
 if [ "$1" == "kovan" ]; then
         GRAPH_NAME="my-swarm/issuance-kovan"
 fi
+if [ "$1" == "mumbai" ]; then
+        GRAPH_NAME="my-swarm/issuance-mumbai"
+fi
+if [ "$1" == "polygon" ]; then
+        GRAPH_NAME="my-swarm/issuance-polygon"
+fi
 
 graph deploy --node https://api.thegraph.com/deploy/ --ipfs https://api.thegraph.com/ipfs/ $GRAPH_NAME --access-token $(cat .access-token)
